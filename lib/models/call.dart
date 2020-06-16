1050d51cff9a9732ab6bc8a6be821038b3131a22
sk1fy <streyking@mail.ru>
2020-06-16 13:11:04 +0800

    meh

--- lib/models/call.dart
+++ lib/models/call.dart
@@ -3,17 +3,15 @@ part 'call.g.dart';
 
 @JsonSerializable()
 class Call {
-  String address, comment;
+  String address;
   DateTime date;
-  List<DateTime> connectedCalls;
 
-  Call({this.date, this.address, this.comment, this.connectedCalls});
+  Call({this.date, this.address});
 
   factory Call.rand() {
     return Call(
         date: DateTime.now(),
         address: "ул.Байкальская 9",
-        comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     );
   }
 
